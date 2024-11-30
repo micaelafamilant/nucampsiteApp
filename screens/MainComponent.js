7a846ffdc08291e7c31a4563d37a32e9e2a88db6
Micaela <mmlfamilant@gmail.com>
2024-11-29 23:33:59 -0500

    Secure Store and Login

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -24,6 +24,7 @@ import { fetchPromotions } from "../features/promotions/promotionsSlice";
 import { fetchPartners } from "../features/partners/partnersSlice";
 import ReservationScreen from "./ReservationScreen";
 import FavoritesScreen from "./FavoritesScreen";
+import LoginScreen from "./LoginScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -151,6 +152,29 @@ const FavoritesNavigator = () => {
   );
 };
 
+const LoginNavigator = () => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <Icon
+              name="sign-in"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
 
@@ -216,6 +240,21 @@ const Main = () => {
         screenOptions={{ drawerStyle: { backgroundColor: "#CEC8FF" } }}
         drawerContent={CustomDrawerContent}
       >
+        <Drawer.Screen
+          name="Login"
+          component={LoginNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="sign-in"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
         <Drawer.Screen
           name="HomeNav"
           component={HomeNavigator}
