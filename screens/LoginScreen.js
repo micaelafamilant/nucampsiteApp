dbdcb906aad1a311b7a57b3ebd91a90b4b9b1f49
Micaela <mmlfamilant@gmail.com>
2024-12-01 13:23:28 -0500

    Get image from gallery

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -161,6 +161,23 @@ const RegisterTab = () => {
     setImageUrl(processedImage.uri);
   };
 
+  const getImageFromGallery = async () => {
+    const mediaLibraryPermissions =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (mediaLibraryPermissions.status === "granted") {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [1, 1],
+      });
+
+      if (capturedImage.assets) {
+        console.log(capturedImage.assets[0]);
+        processImage(capturedImage.assets[0].uri);
+      }
+    }
+  };
+
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -171,6 +188,7 @@ const RegisterTab = () => {
             style={styles.image}
           />
           <Button title="Camera" onPress={getImageFromCamera} />
+          <Button title="Gallery" onPress={getImageFromGallery} />
         </View>
         <Input
           placeholder="Username"
